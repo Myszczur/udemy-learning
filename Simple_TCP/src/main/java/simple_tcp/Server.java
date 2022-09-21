b0b1dbcb94c1313ad2a6ba481c9291360256ff61
Kamil <Kamil.Urbanik@o2.pl>
2022-09-21 16:53:20 +0200

    #0001 - initial commit

---
+++ Simple_TCP/src/main/java/simple_tcp/Server.java
@@ -0,0 +1,16 @@
+package simple_tcp;
+
+public class Server {
+
+    public Server() {
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Server();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
