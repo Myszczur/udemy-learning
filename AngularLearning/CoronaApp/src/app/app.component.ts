5396a5cd2bcaa9044d2b4562a3ae8fe27a805656
Kamil <Kamil.Urbanik@o2.pl>
2022-09-26 15:40:27 +0200

    #0019 - corona app add summary world

--- AngularLearning/CoronaApp/src/app/app.component.ts
+++ AngularLearning/CoronaApp/src/app/app.component.ts
@@ -18,6 +18,5 @@ export class AppComponent {
         console.log(this.posts);
       });
   }
-
   title = 'CoronaApp';
 }
--- AngularLearning/CoronaApp/src/app/app.module.ts
+++ AngularLearning/CoronaApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
