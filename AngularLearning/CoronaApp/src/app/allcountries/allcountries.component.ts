2aadfdc1405dfb0493c771ad1fa3dfa7d674379a
Kamil <Kamil.Urbanik@o2.pl>
2022-09-27 11:53:42 +0200

    #0021 - US summary

---
+++ AngularLearning/CoronaApp/src/app/allcountries/allcountries.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnChanges, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-allcountries',
+  templateUrl: './allcountries.component.html',
+  styleUrls: ['./allcountries.component.css']
+})
+export class AllcountriesComponent implements OnChanges {
+  @Input() allCountriesData:any;
+
+  constructor() {
+  }
+
+  ngOnChanges() {
+    console.log(this.allCountriesData);
+  }
+
+}
--- AngularLearning/CoronaApp/src/app/app.component.ts
+++ AngularLearning/CoronaApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,13 +7,20 @@ import {HttpClient, HttpParams} from "@angular/common/http";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  globalData: any;
+  globalData: any = {};
+  allcountryData: any = {};
+  usData: any = {};
 
   constructor(public http: HttpClient) {
     this.http.get('https://api.covid19api.com/summary')
       .subscribe((value: any) => {
-        console.log(value);
         this.globalData = value.Global;
+        this.allcountryData = value.Countries;
+        this.allcountryData.forEach((countryList: any) => {
+          if (countryList.CountryCode == "US") {
+            this.usData = countryList;
+          }
+        });
       });
   }
 
