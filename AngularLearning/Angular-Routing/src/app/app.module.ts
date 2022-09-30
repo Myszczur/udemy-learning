581f9194c330f9e759972ad34fb91d54d6a21757
Kamil <Kamil.Urbanik@o2.pl>
2022-09-30 14:27:42 +0200

    #0044 - angular routing add components

--- AngularLearning/Angular-Routing/src/app/app.module.ts
+++ AngularLearning/Angular-Routing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
