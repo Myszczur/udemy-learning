23feff437974bc61176b1e55776d4114171c60e3
Kamil <Kamil.Urbanik@o2.pl>
2022-09-30 14:48:28 +0200

    #0045 - angular routing add routing

--- AngularLearning/Angular-Routing/src/app/app-routing.module.ts
+++ AngularLearning/Angular-Routing/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {BlogComponent} from "./components/blog/blog.component";
+import {AboutComponent} from "./components/about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- AngularLearning/Angular-Routing/src/app/app.module.ts
+++ AngularLearning/Angular-Routing/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AppRoutingModule
   ],
   providers: [],
