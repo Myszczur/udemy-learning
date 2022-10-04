1b249b776591ece41e3da69856f181d5ddff0a4e
Kamil <Kamil.Urbanik@o2.pl>
2022-10-04 12:11:05 +0200

    #0055 - add details of hero

--- HeroApp/src/app/heroes/heroes.component.ts
+++ HeroApp/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,12 @@ import {Hero} from "../hero";
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  heroes = HEROES;
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
   constructor() {
   }
@@ -19,4 +22,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelected(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
