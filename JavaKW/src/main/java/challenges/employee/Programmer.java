4157063f381188e1ed85052967ebc714af248bbb
Kamil <Kamil.Urbanik@o2.pl>
2022-10-06 10:51:47 +0200

    #0069 - employee challenge

---
+++ JavaKW/src/main/java/challenges/employee/Programmer.java
@@ -0,0 +1,8 @@
+package challenges.employee;
+
+public class Programmer extends Employee{
+
+    public Programmer(String name, String surname, String languages) {
+        super(name, surname);
+    }
+}
